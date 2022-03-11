3225de5db95f153e9264156d41523f0a38dd692d
Annette Warimwe <annettewarimwe@gmail.com>
2022-03-11 17:20:42 +0300

    Add the show and hide function to the js to affect the "what we do" section

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,31 @@
 // Business logic
+function hide(shownElement, hiddenElement) {
+  shownElement.click(function () {
+    shownElement.hide();
+    hiddenElement.show();
+  });
+}
+
+// function
 
 // UI logic
 $(document).ready(function () {
+  var designIcon = $("#design-icon");
+  var designParagraph = $("#design-paragraph");
+  var developmentIcon = $("#development-icon");
+  var developmentParagraph = $("#development-paragraph");
+  var productIcon = $("#product-icon");
+  var productParagraph = $("#product-paragraph");
+
+  hide(designIcon, designParagraph);
+  hide(designParagraph, designIcon);
+
+  hide(developmentIcon, developmentParagraph);
+  hide(developmentParagraph, developmentIcon);
+
+  hide(productIcon, productParagraph);
+  hide(productParagraph, productIcon);
+
   $("form#message").submit(function (event) {
     event.preventDefault();
   });
